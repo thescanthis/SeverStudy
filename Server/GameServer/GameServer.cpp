c7178b9758b56c7af81bed12a492898d84a234cb
thescanthis <thescanthis@gmail.com>
2025-07-24 19:18:35 +0900

    ProcedureGenerator
    Python xml jinja Library automation Implement.... success..

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -17,6 +17,7 @@
 #include "DBBind.h"
 #include "XmlParser.h"
 #include "DBSynchronizer.h"
+#include "GenProcedures.h"
 
 enum
 {
@@ -46,6 +47,38 @@ int main()
 	DBConnection* dbConn = GDBConnectionPool->Pop();
 	DBSynchronizer dbSync(*dbConn);
 	dbSync.Synchronize(L"GameDB.xml");
+
+	{
+		WCHAR name[] = L"Rookiss";
+		SP::InsertGold insertGold(*dbConn);
+		insertGold.In_Gold(100);
+		insertGold.In_Name(name);
+		insertGold.In_CreateDate(TIMESTAMP_STRUCT{ 2025,7,24 });
+		insertGold.Execute();
+	}
+
+	{
+		SP::GetGold getGold(*dbConn);
+		getGold.In_Gold(100);
+
+		int32 id = 0;
+		int32 gold = 0;
+		WCHAR name[100];
+		TIMESTAMP_STRUCT date;
+
+		getGold.Out_Id(OUT id);
+		getGold.Out_Gold(OUT gold);
+		getGold.Out_Name(OUT name);
+		getGold.Out_CreateDate(OUT date);
+
+		getGold.Execute();
+
+		while (getGold.Fetch())
+		{
+			GConsoleLogger->WriteStdOut(Color::BLUE,
+				L"ID[%d] Gold[%d] Name[%s]\n", id, gold, name);
+		}
+	}
 
 	ClientPacketHandler::Init();
 
